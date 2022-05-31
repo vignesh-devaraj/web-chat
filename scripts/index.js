f45a765358a31e441546716d4a1847984bcec392
Vikkydssv <38271419+Vikkydssv@users.noreply.github.com>
2022-05-31 15:47:11 +0530

    Added data js file for data

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,16 @@
 import { makeProfile } from "./profile.js";
 import { makeFooter } from "./footer.js";
 import { makeChatData } from "./chat-data.js";
+import {userData} from "./data.js"
 
-fetch("../../assets/data.json")
-  .then((data) => data.json())
-  .then((data) => {
-    makeProfile(data?.user);
-    makeFooter(data?.user);
-    makeChatData(data?.user);
-  });
+// fetch("./data.json")
+//   .then((data) => data.json())
+//   .then((data) => {
+//     makeProfile(data?.user);
+//     makeFooter(data?.user);
+//     makeChatData(data?.user);
+//   });
+// console.log(userData);
+makeProfile(userData?.user);
+makeFooter(userData?.user);
+makeChatData(userData?.user);
